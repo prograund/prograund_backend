d2e55682aee510cb5b907fed748954328488fb6e
Humanshu <humanshujaglan@gmail.com>
2024-01-09 14:25:46 +0530

    all done

--- ProGraund_Backend/main/urls.py
+++ ProGraund_Backend/main/urls.py
@@ -4,4 +4,12 @@ from . import views
 urlpatterns = [
     path('posts/', views.all_posts),
     path('posts/<int:id>/', views.all_posts),
+    path('users/', views.all_users),
+    path('users/<int:id>/', views.all_users),
+    path('likes/', views.all_likes),
+    path('likes/<int:id>/', views.all_likes),
+    path('comments/', views.all_comments),
+    path('comments/<int:id>/', views.all_comments),
+    path('follows/', views.all_follows),
+    path('follows/<int:id>/', views.all_follows),
 ]
