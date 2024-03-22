f0ba8fed79ae4809f5fe07fe03945d47b9f7f360
Humanshu Jaglan <140067860+humanshu001@users.noreply.github.com>
2024-03-22 13:56:23 +0530

    added localhost

--- ProGraund_Backend/settings.py
+++ ProGraund_Backend/settings.py
@@ -51,7 +51,8 @@ INSTALLED_APPS = [
 
 CORS_ORIGIN_WHITELIST = [
     'https://prograund.vercel.app',
-        "http://172.19.2.144"
+        "http://172.19.2.144",
+        "http://localhost:3000"
 ]
 
 
