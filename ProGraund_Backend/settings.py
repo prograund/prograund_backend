9fb2cfb79d32b501715df319aaf422a94901ef4d
Humanshu <humanshujaglan@gmail.com>
2024-03-23 21:40:16 +0530

    something

--- ProGraund_Backend/settings.py
+++ ProGraund_Backend/settings.py
@@ -61,8 +61,9 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
     'https://prograund.vercel.app',
-     "http://localhost:3000"
+    # Add other allowed origins if needed
 ]
 
 
